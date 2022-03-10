64c70ebff6e83a1b751375537f7045ee603e3094
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2022-03-10 11:43:28 +0100

    Adding Success and Failure Pages

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const request = require('request')
+// const request = require('request') // obsolete
 const https = require('https')
 
 const app = express()
@@ -40,6 +40,11 @@ app.post('/', function (req, res) {
 	}
 
 	const request = https.request(url, options, function (response) {
+		if (response.statusCode === 200) {
+			res.sendFile(__dirname + '/success.html')
+		} else {
+			res.sendFile(__dirname + '/failure.html')
+		}
 		response.on('data', function (data) {
 			console.log(JSON.parse(data))
 		})
@@ -47,6 +52,10 @@ app.post('/', function (req, res) {
 
 	request.write(jsonData)
 	request.end()
+})
+
+app.post('/failure', function (req, res) {
+	res.redirect('/')
 })
 
 app.listen(3000, function () {
