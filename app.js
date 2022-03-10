32648439b3e4bbac79e1bf95e8218451023422e0
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2022-03-10 11:12:29 +0100

    Posting Data to Mailchimp's Servers via their API

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
+const https = require('https')
 
 const app = express()
 
@@ -12,13 +13,48 @@ app.get('/', function (req, res) {
 })
 
 app.post('/', function (req, res) {
-	var firstName = req.body.fName
-	var lastName = req.body.lName
-	var email = req.body.email
+	const firstName = req.body.fName
+	const lastName = req.body.lName
+	const email = req.body.email
 
-	console.log(firstName, lastName, email)
+	const data = {
+		members: [
+			{
+				email_address: email,
+				status: 'subscribe',
+				merge_fields: {
+					FNAME: firstName,
+					LNAME: lastName,
+				},
+			},
+		],
+	}
+
+	const jsonData = JSON.stringify(data)
+
+	const url = 'https://us14.api.mailchimp.com/3.0/lists/9aea4abb75'
+
+	const options = {
+		method: 'POST',
+		auth: 'MiguelDominguez:462b88b96c93c2941066cd6d77940b22-us14',
+	}
+
+	const request = https.request(url, options, function (response) {
+		response.on('data', function (data) {
+			console.log(JSON.parse(data))
+		})
+	})
+
+	request.write(jsonData)
+	request.end()
 })
 
 app.listen(3000, function () {
 	console.log('Server is running on port 3000')
 })
+
+// API Key
+// 462b88b96c93c2941066cd6d77940b22-us14
+
+// List id || Audience id
+// 9aea4abb75
